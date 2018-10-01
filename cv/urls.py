6bc0d71e5da820e509ba659865d61c18eba2171d
Matt F <matthewrfree@gmail.com>
2018-10-01 17:28:03 -0600

    Add Basic Job history page

--- cv/urls.py
+++ cv/urls.py
@@ -8,4 +8,6 @@ app_name = 'cv'
 urlpatterns = [
     #home page
     path('', views.index, name='index'),
+    #show all work experienc
+    path('jobs', views.jobs, name='jobs'),
 ]
--- cv/views.py
+++ cv/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from .models import Job, JobDescription
+
 # Create your views here.
 def index(request):
 	'''The home page for the Resume'''
 	return render(request, 'cv/index.html')
+
+def jobs(request):
+	'''SHow all Work Experience'''
+	jobs = Job.objects.order_by('date_started')
+	descriptions = JobDescription.objects
+	context = {'jobs': jobs, 'descriptions': descriptions}
+	return render(request, 'cv/jobs.html', context)
