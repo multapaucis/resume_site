8e3519c114d2511bb14108c46b465d557b3569ad
Matt F <matthewrfree@gmail.com>
2018-09-30 18:21:02 -0600

    Add Models to Database

--- cv/admin.py
+++ cv/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from cv.models import Job, JobDescription, Education, Skill
 
+admin.site.register(Job)
+admin.site.register(JobDescription)
+admin.site.register(Education)
+admin.site.register(Skill)
 # Register your models here.
--- cv/models.py
+++ cv/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+	"""Previous Jobs"""
+	title = models.CharField(max_length=200, primary_key=True)
+	date_started = models.DateField()
+	date_finished = models.DateField()
+	location = models.CharField(max_length=200)
+	company = models.CharField(max_length=200)
+	
+	def __str__(self):
+		'''Returns a string with the Job Title'''
+		return self.title
+		
+class JobDescription(models.Model):
+	"""Job Descriptors"""
+	job_title = models.ForeignKey('Job', on_delete=models.PROTECT)
+	description = models.TextField()
+	
+	def __str__(self):
+		'''Returns a string representing the model'''
+		return self.description[:50] + "..."
+
+class Education(models.Model):
+	"""Previous Schools"""
+	school_name = models.CharField(max_length=200, primary_key=True)
+	date_started = models.DateField()
+	date_finished = models.DateField()
+	location = models.CharField(max_length=200)
+	degree = models.CharField(max_length=200)
+	
+	def __str__(self):
+		'''Returns a string with the School Nme'''
+		return self.school_name
+
+
+class Skill(models.Model):
+	"""Releveant Skills"""
+	skill_name = models.CharField(max_length=200, primary_key=True)
+	skill_type = models.CharField(max_length=200)
+	
+	def __str__(self):
+		'''Returns a string representing the Skill'''
+		return self.skill_name
