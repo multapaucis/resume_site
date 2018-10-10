a8b460c617c42f6307801f3a470f47afff664ecc
Matt F <matthewrfree@gmail.com>
2018-10-10 17:45:05 -0600

    Prepare Project for Deployment to Heroku

--- resume/settings.py
+++ resume/settings.py
@@ -129,3 +129,23 @@ STATIC_URL = '/static/'
 BOOTSTRAP4 ={
 	'include_jquery': True,
 	}
+
+# Heroku Settings
+if os.getcwd() == '/app':
+	import dj_database_url
+	DATABASES = {
+		'default': dj_database_url.config(default-'postgres://localhost')
+	}
+
+	# Honor the 'X-Forwarded-Proto' header for request.is_secure().
+	SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+	# Allow all host headers
+	ALLOWED_HOSTS = ['*']
+
+	#sTATIC ASSET CONFIGURATION
+	BASE_DIR= os.path.dirname(os.pat.abspath(__file__))
+	STATIC_ROOT = 'staticfiles'
+	STATICFILES_DIRS = (
+		os.path.join(BASE_DIR, 'static'),
+	)
