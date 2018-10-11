bb483d2fe94325b2d7a67a967f6a56543cfd78ed
Matt F <matthewrfree@gmail.com>
2018-10-11 09:27:06 -0600

    Add Favicon Logo to Base.html

--- resume/settings.py
+++ resume/settings.py
@@ -144,8 +144,14 @@ if os.getcwd() == '/app':
 	ALLOWED_HOSTS = ['*']
 
 	#STATIC ASSET CONFIGURATION
-	BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-	STATIC_ROOT = 'staticfiles'
+	BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+	# Static files (CSS, JavaScript, Images)
+	# https://docs.djangoproject.com/en/1.9/howto/static-files/
+	STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+	STATIC_URL = '/static/'
+
+	# Extra places for collectstatic to find static files.
 	STATICFILES_DIRS = (
-		os.path.join(BASE_DIR, 'static'),
+	    os.path.join(BASE_DIR, 'static'),
 	)
